2dfd41113591c639e76c0aceb85a0e37440d7eaf
Tati Valverde <tatiananaumova5@gmail.com>
2025-06-09 14:50:14 -0400

    add string

---
+++ whitebordCoding/string.js
@@ -0,0 +1,21 @@
+//check type return"string message"
+//1 step create variable
+//loop for
+//summarize this elements
+//return this variable
+
+
+function reverseStr(str){
+ if(typeof str !== "string") return 'is not a string';
+if(str.length === 0) return 'empty string'; 
+let rev = "";
+for(let i = str.length - 1; i >= 0; i--){
+rev += str[i];
+} return rev;
+}
+
+console.log(reverseStr("Hello, World!"));
+console.log(reverseStr(""));
+console.log(reverseStr(1));
+console.log(reverseStr("g"));
+
