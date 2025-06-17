5b4a842d7861a551edc82ea3a217739e447468d0
Tati Valverde <tatiananaumova5@gmail.com>
2025-06-17 16:43:51 -0400

    add changes in Palindrome

--- whitebordCoding/stringPalindrome.js
+++ whitebordCoding/stringPalindrome.js
@@ -1,10 +1,19 @@
 
 // A palindrome is a word, phrase, number, or sequence of words that reads the same backward as forward.
+//PseudoCode
+//check type return"string message"
+//1 step create variable
+//summarize this elements
+//return this variable
+
 
 function isPalindrome(str) {
   if (typeof str !== "string") return 'is not a string';
   if (str.length === 0) return 'empty string';
+  //let str2 = str.toLowerCase().replace(/[^a-z0-9]/g, ``)
   
+  //return str2 ===  str2.split('').reverse().join('')
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
   const reversedStr = str.split('').reverse().join('');
   return str === reversedStr;
 }
